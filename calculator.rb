ac3dfb4b4e65d9c67329ff17e4e9b40ec57c5947
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-20 22:52:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ first_number = 10
 second_number = 5
 sum = first_number + second_number
 difference = first_number - second_number
-product
+product = first_number * second_number
+quotient = first_number/second_number
