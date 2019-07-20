666c1969471b90c774be128c08b9fb81d1ca2755
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-20 22:52:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 # Add your variables here
-first_number = 5
-second_number = 4
+first_number = 10
+second_number = 5
 sum = first_number + second_number
 difference = first_number - second_number
+product
